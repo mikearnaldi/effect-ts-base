e46b00f01d488604aa59b28133a8643fba857211
Michael Arnaldi <arnaldimichael@gmail.com>
2021-07-09 22:05:17 +0100

    minor

--- packages/app/src/console.ts
+++ packages/app/src/console.ts
@@ -2,17 +2,15 @@ import * as T from "@effect-ts/core/Effect";
 import { tag } from "@effect-ts/core/Has";
 import type { _A } from "@effect-ts/core/Utils";
 
-export const makeConsole = T.succeedWith(() => {
-  return {
-    log: (s: string) =>
-      T.succeedWith(() => {
-        console.log(s);
-      }),
-  };
-});
+export const makeLiveConsole = T.succeedWith(() => ({
+  log: (s: string) =>
+    T.succeedWith(() => {
+      console.log(s);
+    }),
+}));
 
-export interface Console extends _A<typeof makeConsole> {}
+export interface Console extends _A<typeof makeLiveConsole> {}
 export const Console = tag<Console>();
-export const LiveConsole = makeConsole.toLayer(Console);
+export const LiveConsole = makeLiveConsole.toLayer(Console);
 
 export const { log } = Console.deriveLifted("log");
